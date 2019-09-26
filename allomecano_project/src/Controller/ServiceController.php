35a7b6d152cffd95d967495042680be20ea01cb4
sebastien017 <sbacle20@gmail.com>
2019-09-26 12:25:37 +0200

    ServiceController + méthodes/routes + views

---
+++ allomecano_project/src/Controller/ServiceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    /**
+     * @Route("/services", name="show_services", methods={"GET})
+     */
+    public function showServices()
+    {
+        return $this->render('service/services.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+
+    /**
+     * @Route("/services/{service}", name="show_service", methods={"GET})
+     */
+    public function showGarageByService()
+    {
+        return $this->render('service/service.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+}
