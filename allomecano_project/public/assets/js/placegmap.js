0c806d31e14c807b43f1e67272fa5710f4b64f71
ArtyHDX <art.rybicki84@gmail.com>
2019-10-01 13:31:08 +0200

    recherche address by google, need fix for empty gps form

---
+++ allomecano_project/public/assets/js/placegmap.js
@@ -0,0 +1,31 @@
+var placeSearch, autocomplete, geocoder;
+
+function initAutocomplete() {
+  geocoder = new google.maps.Geocoder();
+  autocomplete = new google.maps.places.Autocomplete(
+    (document.getElementById('user_input_autocomplete_address')), {
+      types: ['geocode']
+    });
+
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function codeAddress(address) {
+  geocoder.geocode({
+    'address': address
+  }, function(results, status) {
+    if (status == 'OK') {
+      // This is the lat and lng results[0].geometry.location
+      $('#gps-coord').text(results[0].geometry.location)
+      // alert(results[0].geometry.location);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function fillInAddress() {
+  var place = autocomplete.getPlace();
+
+  codeAddress(document.getElementById('user_input_autocomplete_address').value);
+}
