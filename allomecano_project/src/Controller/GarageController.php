7e20afdf53ca10108d338e01467f1a7a55ca071f
maximedelpino <maximedelpino@laposte.net>
2019-10-02 13:19:16 +0200

    début recherche par adresse

--- allomecano_project/src/Controller/GarageController.php
+++ allomecano_project/src/Controller/GarageController.php
@@ -6,6 +6,7 @@ use App\Entity\Service;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Garage;
 
 class GarageController extends AbstractController
 {
@@ -24,7 +25,6 @@ class GarageController extends AbstractController
      */
     public function showGarageByServiceAndGps(Request $request)
     {
-    
         // Récupération de l'adresse
         $address = $request->request->get('address');
 
@@ -32,14 +32,21 @@ class GarageController extends AbstractController
         $gps = $request->request->get('gps');
         $gps = strtr($gps, array('(' => '', ')' => ''));
 
+        // Explode des coordonnées gps
+        $gpsCoordsArray = explode(',', $gps, 2);
+        $latitude = $gpsCoordsArray[0];
+        $longitude = trim($gpsCoordsArray[1]);
+
         // ID du service
         $service_search = $request->request->get('service_search');
         $serviceID = $service_search['name'];
 
-        dump($request); dump($gps); dump($address); dump($serviceID); exit;
+        $garagesJson = $this->getDoctrine()->getRepository(Garage::class)->getGarages();
 
         return $this->render('garage/search_results.html.twig', [
-             
+             'zoomLatitude' => $latitude,
+             'zoomLongitude' => $longitude,
+             'garages' => $garagesJson
         ]);
         
     }
