2a887d30a6f440d6e8bac55b34a97e6a1093c8c1
ArtyHDX <art.rybicki84@gmail.com>
2019-10-04 09:45:21 +0200

    correction gps inscription garage

--- allomecano_project/public/assets/js/placeGmap.js
+++ allomecano_project/public/assets/js/placeGmap.js
@@ -22,6 +22,7 @@ function codeAddress(address) {
       // This is the lat and lng results[0].geometry.location
       $('#gps-coord').val(results[0].geometry.location)
       $('#user_gps').val(results[0].geometry.location)
+      $('#garage_gps').val(results[0].geometry.location)
       //  alert(results[0].geometry.location.lng)
       // alert(results[0].geometry.location);
     } else {
