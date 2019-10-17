5a955f32a3c9ababea1b64048fcf292c03434bf8
Coupecips <ruellens@gmail.com>
2019-10-17 11:13:28 +0200

    suppression de la route presentation

--- allomecano_project/src/Controller/MainController.php
+++ allomecano_project/src/Controller/MainController.php
@@ -65,13 +65,5 @@ class MainController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
-    /**
-     * @Route("/presentation", name="presentation", methods={"GET"})
-     */
-    public function showPresentation()
-    {
-        return $this->render('home/presentation.html.twig', [
-            'controller_name' => 'HomeController', 
-        ]);
-    }
+    
 }
